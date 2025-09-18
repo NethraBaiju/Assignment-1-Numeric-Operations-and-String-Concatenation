3de65e9fa360782185ea6de884486ecafb05448b
NethraBaiju <nethra.b007@gmail.com>
2025-09-18 14:07:39 +0400

    Assignment_week2.py

--- Assignment_week2.py
+++ Assignment_week2.py
@@ -5,7 +5,9 @@
 
 Number_1 = 4
 Number_2 = 0
+print("\r")
 print("Arithametic operations");
+print("\r")
 
 #Addition
 print("Addition :-" ,Number_1,"+" ,Number_2,"=",Number_1 + Number_2)
@@ -22,8 +24,13 @@ if Number_2 != 0:
 
 else :
     print("Division :- Undefined (Division by zero)")
+    print("\r")
 
 # Concatenates the two inputs as strings
 Concatenates = str(Number_1) + str(Number_2);
 print("Concatenate")
+print("\r")
+
 print(Number_1 ,"+", Number_2,"=",Concatenates);
+print("\r")
+
